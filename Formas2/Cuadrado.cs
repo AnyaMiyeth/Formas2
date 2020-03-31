c80291a22ef1e0d2b90751b6dfa375be28f1aa74
anyamiyeth <anyamiyeth@byasystems.com.co>
2020-03-31 15:08:15 -0500

    Agregar archivos de proyecto.

---
+++ Formas2/Cuadrado.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formas2
+{
+    class Cuadrado : Figura
+    {
+        public double Lado { get; set; }
+        public double Area { get; set; }
+
+        public Cuadrado(double lado)
+        {
+            Lado = lado;
+        }
+        public override double CalcularArea()
+        {
+             
+            return Lado * Lado;
+        }
+
+        public override string ToString()
+        {
+            
+            return ($"El area del Cuadrado es{CalcularArea()}");
+        }
+
+    }
+}
